a76ecfdb470e025d8d0ae7935e63ab5be0e75a4e
Dan F <dan@proinbox.com>
2026-01-21 18:14:29 -0500

    alpha version with sortable tables

--- bidsummarize.meta.js
+++ bidsummarize.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name	bidsummarize
 // @description	Analyze bid information to be more informative
+// @license	GPLv3
 // @version	0.9.1
 // @match	https://flightops.inside.ups.com/Dotnet/CrewApps/Bidding/ViewBidSummary.aspx
 // @grant	none
 // @inject-into	content
-// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.user.js
-// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.meta.js
+// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.user.js
+// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.meta.js
 // ==/UserScript==
 // AUTO-GENERATED, do not edit
