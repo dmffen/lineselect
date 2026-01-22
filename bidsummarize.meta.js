9bc916dde316a78991a2a560da81f2658ba9a8ab
Dan F <dan@proinbox.com>
2026-01-21 22:02:24 -0500

    Squashed commit of the following:
        colorscheme, non-hardcoded column sort indicators
        alpha version with sortable tables

--- bidsummarize.meta.js
+++ bidsummarize.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name	bidsummarize
 // @description	Analyze bid information to be more informative
-// @version	0.9.1
+// @license	GPLv3
+// @version	1.0.1-alpha
 // @match	https://flightops.inside.ups.com/Dotnet/CrewApps/Bidding/ViewBidSummary.aspx
 // @grant	none
 // @inject-into	content
-// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.user.js
-// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.meta.js
+// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.user.js
+// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.meta.js
 // ==/UserScript==
 // AUTO-GENERATED, do not edit
--- bidsummarize.user.js
+++ bidsummarize.user.js
@@ -1,15 +1,73 @@
 // ==UserScript==
 // @name	bidsummarize
 // @description	Analyze bid information to be more informative
-// @version	0.9.1
+// @license	GPLv3
+// @version	1.0.1-alpha
 // @match	https://flightops.inside.ups.com/Dotnet/CrewApps/Bidding/ViewBidSummary.aspx
 // @grant	none
 // @inject-into	content
-// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.user.js
-// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/refs/heads/dev/bidsummarize.meta.js
+// @downloadURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.user.js
+// @updateURL	https://raw.githubusercontent.com/dmffen/lineselect/dev/bidsummarize.meta.js
 // ==/UserScript==
 (() => {
 	'use strict';
+
+	const Sortable = class {
+		static {
+			this.strcmp = Intl.Collator(undefined, {"sensitivity":"base"}).compare;
+			['NOSORT', 'NUMERIC', 'STRING'].forEach((name, index) =>
+				Object.defineProperty(this, name, {value:index})
+			);
+		}
+
+		constructor(...coldef) {
+			this.table = document.createElement('table');
+			this.data = [];
+			this.sortkey = undefined;
+			let group = this.table.appendChild(document.createElement('colgroup'));
+			let head = this.table.appendChild(document.createElement('thead'));
+
+			coldef.forEach(([name, sorting = Sortable.NUMERIC], index) => {
+				group.appendChild(document.createElement('col')).setAttribute('name', name);
+
+				let cell = head.appendChild(document.createElement('th'));
+				cell.textContent = name;
+				if (sorting != Sortable.NOSORT) {
+					cell.addEventListener('click', e => this.sortBy(sorting, index + 1));
+					cell.className = 'sortable';
+				}
+			});
+
+			this.body = this.table.appendChild(document.createElement('tbody'));
+		}
+
+		sortBy(sorting, element) {
+			if (Math.abs(this.sortkey) == element) {
+				this.data.reverse();
+				this.sortkey *= -1;
+			} else {
+				this.sortkey = element;
+				if (sorting === Sortable.NUMERIC) {
+					this.data.sort((a, b) => a[element] - b[element]);
+				} else {
+					this.data.sort((a, b) => Sortable.strcmp(a[element], b[element]));
+				}
+				this.table.querySelectorAll('col.sort').forEach(col => col.classList.remove('sort'));
+				this.table.querySelector(`col:nth-child(${element})`).classList.add('sort');
+			}
+
+			this.data.forEach(record => this.body.appendChild(record[0]));
+		}
+
+		addData(newdata) {
+			newdata.forEach(record => {
+				let row = this.body.insertRow();
+				record.forEach(field => row.insertCell().innerHTML = field);
+				this.data.push([row, ...record]);
+			});
+		}
+	};
+
 	const userid = document.getElementById('FltOpsHeader1_lblGEMSID').textContent;
 	let srcdata = document.getElementById('cphBody_DisplayTextFile1_lblContent');
 	let output = document.createElement('div');
@@ -28,8 +86,10 @@
 		table._bidtbl.hide {display:none}
 		table._bidtbl td {padding:0.25em}
 		table._bidtbl td:nth-child(3) {white-space:nowrap}
-		table._bidtbl thead {font-weight:bold}
-		table._bidtbl tbody tr:nth-child(odd) {background-color:#ddb}
+		table._bidtbl thead {font-weight:bold; white-space:nowrap; border-bottom:1px solid black; text-align:left;}
+		table._bidtbl thead th.sortable::before {content:'⇅ '}
+		table._bidtbl tbody tr:nth-child(odd) {background-color:#2022}
+		table._bidtbl col.sort {background-color:#cfff}
 		div._bidsummary {
 			font-size:larger;
 			font-weight:bold;
@@ -40,7 +100,7 @@
 		div._bidsummary::before {content:'▼ '}
 		div._bidsummary.hide::before {content:'▷ '}`
 	);
-	document.adoptedStyleSheets = [addstyle];
+	document.adoptedStyleSheets.push(addstyle);
 
 	function tbl_toggle(hdr, tbl) {
 		hdr.classList.toggle('hide');
@@ -55,7 +115,7 @@
 		const re1 = /^ONLY PILOTS WITH BIDS ON FILE AS OF (..\/..\/.. AT ..:..Z)/;
 		const re2 = /^NAME\s.*(SCHEDULE|SYSTEM) BIDS.*Page\s+(\d+)/;
 		// groups:    1 name                   2 emp.  3 sen.  4 base                5 A/C.  6 pos.       7 bidlist
-		const re3 = /^([A-Z][\w-',. ]+[A-Z])\s+(\d+)\s+(\d+)\s+(ANC|MIA|ONT|SDFZ?)\s+(\w+)\s+(CPT|F\/O)\s+([\d ]+)/;
+		const re3 = /^([A-Z][\w-',. ]+[A-Z])\s+(\d+)\s+(\d+)\s+(ANC|MIA|ONT|SDFZ?)\s+(\w+)\s+(CPT|F\/?O)\s+([\d ]+)/;
 		const re4 = /^[\d ]+$/;
 
 		while (lines.length) {
@@ -68,18 +128,17 @@
 			} else if (found = line.match(re2)) {
 				if (found[1] == "SYSTEM") break;
 				pages = found[2];
-			} else if (found = line.match(re3)) {
+			} else if (found = line.replace('/', '').match(re3)) {
 				let crewpos = found.slice(4, 7).join('_');
 				let bidlist = found[7].split(/\s+/).map(n => Number.parseInt(n));
 				let list;
 				if (people.has(crewpos)) {
 					list = people.get(crewpos);
 				} else {
-					list = [];
-					people.set(crewpos, list);
+					people.set(crewpos, list = []);
 				}
 				// person: 0 sen.    1 emp.    2 name.   3 hold 4 bids
-				list.push([found[3], found[2], found[1], -1,    bidlist]);
+				list.push([Number.parseInt(found[3]), Number.parseInt(found[2]), found[1], -1,    bidlist]);
 				previous = bidlist;
 				if (found[2]==userid) show=crewpos;
 			} else if (found = line.match(re4)) {
@@ -96,49 +155,39 @@
 				let bids = person.pop();
 				let hold = bids.find(line => !taken.has(line));
 				person.push(bids.map(line => `<span class=${taken.has(line) ? "dim" : ""}>${line}</span>`).join(' '));
-				if (hold === undefined) {
-					console.log(crewpos + ' ' + person.slice(0, 3).join() + ' underbid');
-				} else {
+				if (hold != undefined) {
 					taken.add(hold);
 					person[3] = hold;
 				}
 			})
 		});
 
-		output.innerHTML = '<div class="_bidinfo">Tap/click a bid group to expand or collapse</div>';
+		output.className="_bidinfo";
+		output.textContent = 'Tap/click a bid group to expand/collapse. Tap column headings to sort a table.';
 
-		[...people.keys()].sort().forEach(crewpos => {
+		Array.from(people.keys()).sort().forEach(crewpos => {
 			let hideclass = ((show==crewpos)?'':' hide');
-			let tbl = document.createElement('table');
-			tbl.id = 'bid_' + crewpos;
-			tbl.className = '_bidtbl' + hideclass;
-			tbl.appendChild(document.createElement('tbody'));
-			people.get(crewpos).forEach(person => {
-				let row = tbl.insertRow();
-				person.forEach(field => {row.insertCell().innerHTML = field});
-			});
-
-			let header = document.createElement('thead');
-			let row = header.insertRow();
-			["Sen", "Emp", "Name", "Hold", "Bids"].forEach(h => {
-				let th = row.insertCell();
-				th.textContent = h;
-				//TODO: th.onclick = () => {console.log(`sort ${crewpos} by ${h}`)};
-			});
-			tbl.prepend(header);
+			let sorter = new Sortable(['Sen'],['Emp'],['Name',Sortable.STRING],['Hold'],['Bids',Sortable.NOSORT]);
+			sorter.table.id = 'bid_' + crewpos;
+			sorter.table.className = '_bidtbl' + hideclass;
+			sorter.addData(people.get(crewpos));
+			sorter.sortBy(Sortable.NUMERIC, 1);
 
 			let caption = document.createElement('div');
 			caption.className = '_bidsummary' + hideclass;
 			caption.textContent = crewpos;
-			caption.onclick = () => tbl_toggle(caption, tbl);
+			caption.onclick = () => tbl_toggle(caption, sorter.table);
 
 			output.appendChild(caption);
-			output.appendChild(tbl);
+			output.appendChild(sorter.table);
 		});
 
 		let caption = document.createElement('div');
 		caption.className = '_bidinfo';
-		caption.textContent = `Read ${pages} pages of bids dated "${prepared}" in ${Date.now() - start}ms`;
+		caption.innerHTML =
+`Note: This summary doesn't know what bid numbers are valid or how many people
+have not bid.<br>A "Hold" number of -1 means that person underbid.<br>Read ${pages}
+pages of bids dated "${prepared}" in ${Date.now() - start}ms`;
 		output.appendChild(caption);
 	};
 })();
